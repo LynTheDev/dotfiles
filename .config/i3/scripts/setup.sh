37561fb0009bc760e7e8f42c8e5dcaa1a8068869
LynTheDev <98173891+LynTheDev@users.noreply.github.com>
2023-08-15 02:53:34 +0300

    init commit

---
+++ .config/i3/scripts/setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "This script has been made for arch ONLY. All this does is install dependencies."
+echo -n "Continue? (Y/n)"
+read -r ANSWER
+
+case $ANSWER in
+	y)
+		echo "Continuing."
+	;;
+	Y)
+		echo "Continuing."
+	;;
+	*)
+		echo "Aborting."
+		exit 0
+	;;
+esac
+
+echo "Installing packages.."
+sudo pacman -S rofi flameshot feh picom arandr pcmanfm xarchiver i3 polybar dunst lxappearance 
+
+echo "Would you like to install xfce4-terminal, or konsole? (1/2)"
+read -r TERM
+
+case $TERM in
+    1)
+		sudo pacman -S xfce4-terminal
+    ;;
+    2)
+		sudo pacman -S konsole
+    ;;
+    *)
+		echo "Unknown, continuing without a terminal."
+    ;;
+esac
+
+echo "Installing fonts..."
+sudo pacman -S ttf-font-awesome ttf-agave-nerd
+
+echo "Setting up rofi.."
+git clone --depth 1 https://github.com/adi1090x/rofi
+cd rofi
+chmod +x ./setup.sh
+
+./setup.sh
+
+cd ..
+rm -rf rofi
+
+echo "Done. Please run arandr and replace screen_layout.sh with your current setup."
+echo "dont forget to cp the contents of the base folder to you .config!"
+echo "Install firefox if you do not have it, and a display manager of your choice."
+echo "Do not forget to install a gtk3 theme and icons! Happy new system~"
--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 # I3 Dotfiles
-WIP
+
+# NOTE:
+Please keep in mind that this was made for myself. I do not know how your pc will react to this.
+
+# Setup:
+```bash
+    git clone https://github.com/LynTheDev/dotfiles
+    cd dotfiles/.config/i3/scripts
+    chmod +x ./setup.sh
+    ./setup.sh
+```
+Now, you must copy the configs to your .config directory.
+
+```bash
+    cd /path/to/dotfiles/.config
+    cp -r * ~/.config
+```
+
+Done, Boot into the i3 install and get yourself a nice gtk theme.
+Enjoy~
+
+# Utils:
+Run your apps with rofi, by pressing `mod+d`.
+
+Screenshot using flameshot by pressing `mod+shift+s`.
+
+Open a terminal by pressing `mod+enter`.
+
+# Screenshot:
 
 ![image](https://github.com/LynTheDev/dotfiles/assets/98173891/bcccd5f5-e848-4278-8c6b-c4f9b5ef2174)
